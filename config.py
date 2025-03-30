b99bb5adba886211ec71b3df2c5f088b62556983
Konstantin <kuznetsovkm@yandex.ru>
2025-03-30 18:07:28 +0300

    векторстрой

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import time
 
-DEGREE_OF_CLUTTER = 4 #степень загроможденности
-# PATH_ = 'BASHNEFT/'
-PATH_ = 'TATNEFT/'
+DEGREE_OF_CLUTTER = 3 #степень загроможденности
+PATH_ = 'BASHNEFT/'
+# PATH_ = 'TATNEFT/'
+# PATH_ = 'NPF/'
+# PATH_ = 'VECTORSTROY/'
 INIQ_TEXT = str(int(time.time()))
