d406c29bd1e137a6bed7b27f8bd60d2efb71c3ff
KONSTANTIN <kuznetsovkm@yandex.ru>
2025-07-12 08:30:40 +0300

    Добавлен шаблон с трубопроводами

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import time
 
 DEGREE_OF_CLUTTER = 3 #степень загроможденности
 # PATH_ = 'BASHNEFT/'
-PATH_ = 'BASHNEFT_with_line/'
+PATH_ = 'BASHNEFT_state_with_line/'
 # PATH_ = 'SHESHMAOIL/'
 # PATH_ = 'TATNEFT/'
 # PATH_ = 'NPF/'
