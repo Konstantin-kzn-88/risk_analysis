2378b6d1ec563425c47bb135eb0866781b9b0620
KONSTANTIN <kuznetsovkm@yandex.ru>
2025-07-17 21:12:19 +0300

    Шаблоны Оренбургнефть

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import time
 
-DEGREE_OF_CLUTTER = 4 #степень загроможденности
+DEGREE_OF_CLUTTER = 3 #степень загроможденности
 # PATH_ = 'BASHNEFT/'
 # PATH_ = 'BASHNEFT_state_with_line/'
-PATH_ = 'BASHNEFT_with_line/'
+# PATH_ = 'BASHNEFT_with_line/'
 # PATH_ = 'SHESHMAOIL/'
 # PATH_ = 'TATNEFT/'
 # PATH_ = 'NPF/'
 # PATH_ = 'VECTORSTROY/'
+PATH_ = 'ORENBURGNEFT_state/'
 INIQ_TEXT = str(int(time.time()))
